46d4d9ad42e25f60412f24b3788f5ce70593ebda
dolevMicrosoft <dolevbrami2005@gmail.com>
2026-03-03 15:26:44 +0000

    removed post_installation

--- post_installations.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-apt update
-apt install -y ros-humble-turtlesim
-
-chmod 600 /root/.ssh/id_ed25519
-eval \"$(ssh-agent -s)\"
-ssh-add /root/.ssh/id_ed25519
-git config --global core.sshCommand 'ssh -i /root/.ssh/id_ed25519 -o StrictHostKeyChecking=yes'
