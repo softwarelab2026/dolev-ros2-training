161a2e14318743d5d39a79ed11b4f9df82d032ac
dolevMicrosoft <dolevbrami2005@gmail.com>
2026-03-08 12:06:07 +0200

    removed them from branch

--- src/main.py
+++
@@ -1,9 +0,0 @@
-def add(x: int, y: int) -> int:
-    return x + y
-
-
-if __name__ == "__main__":
-    a = 5
-    b = 10
-    result = add(a, b)
-    print(f"The result of adding {a} and {b} is: {result}")
--- tests/test_main.py
+++
@@ -1,5 +0,0 @@
-from src.main import add
-
-
-def test_add_numbers() -> None:
-    assert add(2, 3) == 5
