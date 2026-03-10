d08bde6bad524d2545e088a560a9aee99fb8861a
dolevMicrosoft <dolevbrami2005@gmail.com>
2026-03-10 18:12:07 +0000

    made  ahuge refactor aboutframe generator

--- ros2_ws/src/ball_tracking_system/ball_tracking_system/nodes/camera.py
+++ ros2_ws/src/ball_tracking_system/ball_tracking_system/nodes/camera.py
@@ -13,19 +13,20 @@ class CameraSimNode(Node):
         self.publisher_ = self.create_publisher(Image, "/camera/image_raw", 10)
         self.cv_bridge = CvBridge()
         self.FPS = 10
+
         self.video_width = 1280
         self.video_height = 960
+
         self.timer = self.create_timer(1.0 / self.FPS, self.timer_callback)
+        self.frame = FrameGenerator(self.video_width, self.video_height, ball_radius=20)
 
     def timer_callback(self):
-        frame = FrameGenerator(self.video_width, self.video_height, ball_radius=20)
+        self.frame.move_objects()
+        self.frame.generate_frame()
 
-        frame.generate_frame()
-
-        # Convert to ROS Image message
-        image_msg = self.cv_bridge.cv2_to_imgmsg(frame.data, encoding="bgr8")
+        image_msg = self.cv_bridge.cv2_to_imgmsg(self.frame.data, encoding="bgr8")
         self.publisher_.publish(image_msg)
-        self.get_logger().info(f"Publishing frame with ball at {self.ball.pos}")
+        self.get_logger().info(f"Publishing frame with ball at {self.frame.ball_pos}")
 
 
 def main(args=None):
