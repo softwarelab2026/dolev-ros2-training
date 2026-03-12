b02a671b4577b66bf5e48e3ddb6582f67dda95f6
dolevMicrosoft <dolevbrami2005@gmail.com>
2026-03-12 16:32:13 +0000

    private varibles

--- ros2_ws/src/ball_tracking_system/ball_tracking_system/logic/frame_generator.py
+++ ros2_ws/src/ball_tracking_system/ball_tracking_system/logic/frame_generator.py
@@ -4,9 +4,10 @@ import cv2
 
 class FrameGenerator:
     def __init__(self, width, height, ball_radius):
-        self.width = width
-        self.height = height
-        self.ball_radius = ball_radius
+        self._width = width
+    
+        self._height = height
+        self._ball_radius = ball_radius
 
         self.ball_pos = [width // 2, height // 2]
         self.ball_val = [5, 3]
@@ -21,23 +22,23 @@ class FrameGenerator:
         self.ball_pos[1] += self.ball_val[1]
 
         if (
-            self.ball_pos[0] <= self.ball_radius
-            or self.ball_pos[0] >= self.width - self.ball_radius
+            self.ball_pos[0] <= self._ball_radius
+            or self.ball_pos[0] >= self._width - self._ball_radius
         ):
             self.ball_val[0] = -self.ball_val[0]
 
         if (
-            self.ball_pos[1] <= self.ball_radius
-            or self.ball_pos[1] >= self.height - self.ball_radius
+            self.ball_pos[1] <= self._ball_radius
+            or self.ball_pos[1] >= self._height - self._ball_radius
         ):
             self.ball_val[1] = -self.ball_val[1]
 
     def generate_frame(self):
-        self.data = np.ones((self.height, self.width, 3), dtype=np.uint8) * 255
+        self.data = np.ones((self._height, self._width, 3), dtype=np.uint8) * 255
         cv2.circle(
             self.data,
             (int(self.ball_pos[0]), int(self.ball_pos[1])),
-            self.ball_radius,
+            self._ball_radius,
             (0, 0, 255),
             -1,
         )
