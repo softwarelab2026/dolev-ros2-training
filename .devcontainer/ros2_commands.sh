9fdbce487705a2d2fb0998c702cf3d7fb8aeebdb
dolevMicrosoft <dolevbrami2005@gmail.com>
2026-03-12 09:52:10 +0000

    where is my commit

--- .devcontainer/ros2_commands.sh
+++ .devcontainer/ros2_commands.sh
@@ -1,5 +1,4 @@
 cd ros2_ws
 echo "source /opt/ros/humble/setup.bash" >> /etc/bash.bashrc
 colcon build --symlink-install
-
 echo 'source /workspaces/dolev-ros2-training/ros2_ws/install/setup.bash' >> ~/.bashrc
